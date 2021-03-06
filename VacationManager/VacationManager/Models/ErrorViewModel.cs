37f67242e1e69814a8443d00027688cb53b08cf8
ViktorKapra <73069830+ViktorKapra@users.noreply.github.com>
2021-03-06 10:38:05 +0200

    Add empty ASP.NET project
    On this file we would build our project.

---
+++ VacationManager/VacationManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VacationManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
