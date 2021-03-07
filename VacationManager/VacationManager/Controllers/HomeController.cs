46d7f1d0015018d1df90330e7e0453556fabe003
Виктор Капра <viktorkapra@mail.ru>
2021-03-07 17:43:29 +0200

    Created Login view; added Login action in HomeController; added Login button in Index view

--- VacationManager/VacationManager/Controllers/HomeController.cs
+++ VacationManager/VacationManager/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace VacationManager.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
