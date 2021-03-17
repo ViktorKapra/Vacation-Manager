1df7a7d5b2c22ccb9ce21899a8391afaa7bd6ee8
Виктор Капра <viktorkapra@mail.ru>
2021-03-17 19:01:32 +0200

    Added Login functionallity via ASP.NET Core Identity

---
+++ VacationManager/VacationManager/Areas/Identity/Data/VacationManagerUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace VacationManager.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the VacationManagerUser class
+    public class VacationManagerUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LirstName { get; set; }
+    }
+}
---
+++ VacationManager/VacationManager/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VacationManager.Areas.Identity.Data;
+using VacationManager.Data;
+
+[assembly: HostingStartup(typeof(VacationManager.Areas.Identity.IdentityHostingStartup))]
+namespace VacationManager.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<LoginContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("LoginContextConnection")));
+
+                services.AddDefaultIdentity<VacationManagerUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<LoginContext>();
+            });
+        }
+    }
+}
