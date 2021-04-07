c2448ea0ef2cfa65dfaa4689f0bf05d6070f2a77
Виктор Капра <viktorkapra@mail.ru>
2021-04-07 20:57:14 +0300

    Add Edit for PaidTimeOff

--- VacationManager/VacationManager/Controllers/TimeOffsController.cs
+++ VacationManager/VacationManager/Controllers/TimeOffsController.cs
@@ -13,32 +13,34 @@ using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
 
 namespace VacationManager.Controllers
 {
     public class TimeOffsController : Controller
     {
 
-        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly VacationManagerContext _context;
+        private int currentUserId;
 
         private int FindUserId()
         {
-
+            
             var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            
             int userId = _context.Users.FirstOrDefault(u => u.UserName == userEmail).Id;
             return userId;
         }
         public TimeOffsController()
         { 
             _context = new VacationManagerContext();
+
         }
         // GET: TimeOffsController
         public ActionResult Index()
         {
-            
+            currentUserId = FindUserId();
             TimeOffsIndexVM model = new TimeOffsIndexVM();
-            int currentUserId =  FindUserId();
             model.PaidTimeOffs = _context.PaidTimeOffs.Where(pto=> pto.RequestorId==currentUserId).ToList();
             model.UnpaidTimeOffs = _context.UnpaidTimeOffs.Where(pto => pto.RequestorId == currentUserId).ToList();
             model.SickTimeOffs = _context.SickTimeOffs.Where(pto => pto.RequestorId == currentUserId).ToList();
@@ -74,24 +76,60 @@ namespace VacationManager.Controllers
         }
 
         // GET: TimeOffsController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<IActionResult> EditPaid(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var timeOff = await _context.PaidTimeOffs.FindAsync(id);
+            if (timeOff == null)
+            {
+                return NotFound();
+            }
+
+            return View(timeOff);
         }
 
         // POST: TimeOffsController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> EditPaidAsync(int id, [Bind("StartDate,EndDate,IsHalfDay,IsApproved,Id")] PaidTimeOff paidTimeOff)
         {
-            try
+            currentUserId = FindUserId();
+            paidTimeOff.Requestor = _context.Users.FirstOrDefault(u => u.Id == currentUserId);
+            paidTimeOff.RequestorId = currentUserId;
+            paidTimeOff.IsApproved = false;
+            paidTimeOff.CreationDate = DateTime.Now;
+
+            if (id != paidTimeOff.Id)
             {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.PaidTimeOffs.Update(paidTimeOff);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PaidTimeOffExists(paidTimeOff.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+
+            return RedirectToAction("Index");
         }
 
         // GET: TimeOffsController/Delete/5
@@ -113,6 +151,11 @@ namespace VacationManager.Controllers
             {
                 return View();
             }
+        }
+
+        private bool PaidTimeOffExists(int id)
+        {
+            return _context.PaidTimeOffs.Any(e => e.Id == id);
         }
     }
 }
