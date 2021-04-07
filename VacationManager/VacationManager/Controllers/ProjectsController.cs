ac261161001564f05728ccfc63b0a8aba0ac120e
zzblueteamzz <zzblueteamzz27@gmail.com>
2021-04-07 23:11:10 +0300

    projects and teams

--- VacationManager/VacationManager/Controllers/ProjectsController.cs
+++ VacationManager/VacationManager/Controllers/ProjectsController.cs
@@ -20,11 +20,37 @@ namespace VacationManager.Controllers
         }
 
         // GET: Projects
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Projects.ToListAsync());
         }
-
+        [HttpPost]
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
+        {
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "Name" : "";
+            ViewData["DateSortParm"] = String.IsNullOrEmpty(sortOrder) ? "Description" : "";
+            ViewData["CurrentFilter"] = searchString;
+            var project = from s in _context.Projects
+                       select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                project = project.Where(s => s.Name.Contains(searchString) || s.Description.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "Name":
+                    project = project.OrderByDescending(s => s.Name);
+                    break;
+                case "FirstName":
+                    project = project.OrderBy(s => s.Description);
+                    break;
+                default:
+                    project = project.OrderBy(s => s.Name);
+                    break;
+            }
+            return View(await project.ToListAsync());
+        }
         // GET: Projects/Details/5
         public async Task<IActionResult> Details(int? id)
         {
